2138195da34f818769c7f574f25abcc73c712ec2
Darrell Best <dsbest@g.clemson.edu>
2017-04-10 18:53:53 -0400

    updating main driver

--- src/com/angelcraftonomy/disabler/main/MainDriver.java
+++ src/com/angelcraftonomy/disabler/main/MainDriver.java
@@ -2,18 +2,25 @@ package com.angelcraftonomy.disabler.main;
 
 import java.util.logging.Logger;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MainDriver extends JavaPlugin {
 	public static Logger logger;
 	public static MainDriver plugin;
 	private MainCommandExecutor myCommands;
+	private FileConfiguration config;
 
 	@Override
 	public void onEnable() {
 		System.out.println("[Disabler] Enabling Disabler v1.0");
 		logger = this.getLogger();
 		plugin = this;
+		config = this.getConfig();
+
+		config.addDefault("youAreAwesome", true);
+		config.options().copyDefaults(true);
+		saveConfig();
 
 		myCommands = new MainCommandExecutor(this);
 		getCommand("disabler").setExecutor(myCommands);
